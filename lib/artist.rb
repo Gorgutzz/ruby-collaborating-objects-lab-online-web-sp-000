bb2cd3f409472394246f80408c2aaf7594bc4aec
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-14 20:48:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,6 @@
 class Artist
   attr_accessor :name, :songs
+
   @@all = []
 
   def initialize(name)
@@ -11,13 +12,13 @@ class Artist
     @songs
   end
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
-
   def save
     @@all << self
+    self
+  end
+
+  def add_song(song)
+    @songs << song
   end
 
   def self.all
@@ -29,7 +30,6 @@ class Artist
   end
 
   def print_songs
-    songs.each {|song| puts song.name}
+    self.songs.each { |song| puts song.name }
   end
-
 end
