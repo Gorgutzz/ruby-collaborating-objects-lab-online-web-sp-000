f8811d75a5ae8279d1342727585251f35028867a
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-14 19:54:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,19 +4,8 @@ class Song
 
   def initialize(name)
     @name = name
-    @@all << self
   end
 
-  def self.all
-    @@all
-  end
 
-  def artist_name
-    if self.artist
-      self.artist.name
-    else
-      nil
-    end
-  end
 
-end 
+end
