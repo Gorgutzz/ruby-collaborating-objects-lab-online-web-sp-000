9e6b369d4c6f0a59f723f36ddcfe629cad07d195
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-14 20:31:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,9 +8,9 @@ class Song
 
   def self.new_by_filename(filename)
     artist, song = filename.split(" - ")
-    new_song = self.new(song)
-    new_song.artist_name = artist
-    new_song
+    song = self.new(song_name)
+    song.artist_name = artist
+    song
   end
 
   def artist_name(name)
